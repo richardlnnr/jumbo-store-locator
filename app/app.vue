3ffc46fbb6f09d30743879368233feb3f6240bf0
Richard Lindner <richard.lindner@syngenta.com>
2026-04-24 19:16:20 -0300

    feat: add example map box below the welcome cards

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,8 @@
     <UApp>
         <NuxtRouteAnnouncer />
         <NuxtWelcome />
+        <section class="flex flex-col items-center pb-12 px-4">
+            <MapBox class="max-w-[980px] w-full" />
+        </section>
     </UApp>
 </template>
