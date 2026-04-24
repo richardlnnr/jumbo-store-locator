20f1a9f763bde6aef94dc2baae4fc37fd2c39696
Richard Lindner <richard.lindner@syngenta.com>
2026-04-24 17:17:36 -0300

    chore: reformat files to match stylistic config

--- app/app.nuxt.test.ts
+++ app/app.nuxt.test.ts
@@ -3,9 +3,9 @@ import { mountSuspended } from '@nuxt/test-utils/runtime'
 import App from './app.vue'
 
 describe('app.vue', () => {
-  it('Should mount inside the Nuxt runtime', async () => {
-    const component = await mountSuspended(App)
+    it('Should mount inside the Nuxt runtime', async () => {
+        const component = await mountSuspended(App)
 
-    expect(component.html()).toBeTruthy()
-  })
+        expect(component.html()).toBeTruthy()
+    })
 })
