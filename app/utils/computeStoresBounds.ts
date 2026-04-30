7d196c64a7a241a2227e59f7a1679444bc82c25c
Richard Lindner <richard.lindner@syngenta.com>
2026-04-30 20:01:03 -0300

    feat(map): add computeStoresBounds util

---
+++ app/utils/computeStoresBounds.ts
@@ -0,0 +1,18 @@
+import bbox from '@turf/bbox'
+import type { LngLatBoundsLike } from 'mapbox-gl'
+
+import type { JumboStoreFeatureCollection } from '../../shared/types/geojson'
+
+const NETHERLANDS_BOUNDS: LngLatBoundsLike = [
+    [3.31, 50.75],
+    [7.23, 53.55],
+]
+
+export const computeStoresBounds = (
+    collection: JumboStoreFeatureCollection,
+): LngLatBoundsLike => {
+    if (collection.features.length === 0) return NETHERLANDS_BOUNDS
+
+    const [west, south, east, north] = bbox(collection)
+    return [[west, south], [east, north]]
+}
